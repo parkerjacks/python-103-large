7e24e0134615e564fa4b30a700fd39c69f0cc999
Park Jack <parker.jackson.hhs@gmail.com>
2020-04-05 19:37:59 -0400

    created a file for a 100 triangle pattern and called it 100_triangle.py

---
+++ 100_triangle.py
@@ -0,0 +1,14 @@
+
+#Set it Up 
+count = 1 
+material = "*"
+
+#Work it Out 
+for number in range(100 +1): 
+    star_count = material * count
+    star_count = star_count.center(100)
+    print(star_count) 
+    count = count + 1
+
+
+#Finish 
